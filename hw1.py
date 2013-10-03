7f510daf4be8973b81360ed795734276e26af46e
wearya19 <wearya19@ccm-2610-e1.evergreen.edu>
2013-10-03 13:50:05 -0700

    added new homework1 files

---
+++ hw1.py
@@ -0,0 +1,46 @@
+# Name: Ryan Weaver 
+# Evergreen Login: wearya19
+# Computer Science Foundations
+# Programming as a Way of Life
+# Homework 1
+
+# You may do your work by editing this file, or by typing code at the
+# command line and copying it into the appropriate part of this file when
+# you are done.  When you are done, running this file should compute and
+# print the answers to all the problems.
+
+import math                     # makes the math.sqrt function available
+
+
+###
+### Problem 1
+###
+
+print "Problem 1 solution follows:"
+
+print "Hello World"
+
+
+###
+### Problem 2
+###
+
+print "Problem 2 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+
+###
+### Problem 3
+###
+
+print "Problem 3 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+
+###
+### Collaboration
+###
+
+# ... List your collaborators here, as a comment (on a line starting with "#").
